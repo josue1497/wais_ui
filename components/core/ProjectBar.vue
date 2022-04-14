538634d800c08e81b63fc5671ae31cfbb2d34b3b
josuermartinezm <78878134+josuermartinezm@users.noreply.github.com>
2022-04-14 19:17:46 -0400

    Nice

--- components/core/ProjectBar.vue
+++ components/core/ProjectBar.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="px-5 -py-1 border-t border-black m-6 cursor-default">
-    <div class="w-full" >
-        <ul class="w-48 divide-y-2 divide-gray-200">
-          <li class="flex justify-between items-center space-x-6  px-4 h-28">
-                <div class="text-[6rem] leading-[10vh] whitespace-nowrap"  v-on:click="showElement = !showElement">
-                  {{name}}  
-                </div>
-                <div  v-if="showElement" >
-                     <div :class=" ` w-[200px]  ${marginElement(index)}`" v-for="(tag, index) in tags" :key="index" :style="{transform:`rotate(${rotateElement(index)}deg)`}">
-                        <a href="#" class="text-[1.5rem] bg-[#232531] text-[#FBFBFB] mr-2 px-2.5 rounded-xl" >{{tag}}</a>
-                     </div>
-                </div>
-          </li>
-        </ul>   
+  <div class="px-5 border-t border-black m-3 md:m-6 cursor-default">
+    <div class="w-full">
+      <ul class="w-48 divide-y-2 divide-gray-200">
+        <li class="flex flex-col md:flex-row justify-between items-start md:items-center space-x-6  px-4 h-auto md:h-28 text-justify">
+          <div class="text-[2rem] md:text-[6rem] leading-[10vh] text-justify whitespace-nowrap" @click="show">
+            {{name}}
+          </div>
+          <div v-if="visible" class="hidden md:block">
+            <div :class=" ` w-[200px]  ${marginElement(index)}`" v-for="(tag, index) in tags" :key="index"
+                 :style="{transform:`rotate(${rotateElement(index)}deg)`}">
+              <a href="#" class="text-[1.5rem] bg-[#232531] text-[#FBFBFB] mr-2 px-2.5 rounded-xl">{{tag}}</a>
+            </div>
+          </div>
+          <div v-if="visible" class="block md:hidden">
+            <div :class=" `w-full my-3`" v-for="(tag, index) in tags" :key="index">
+              <a href="#" class="text-xl  bg-[#232531] text-[#FBFBFB] px-2.5 rounded-xl">{{tag}}</a>
+            </div>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
 
@@ -24,35 +30,42 @@
         name: "ProjectBar",
         props: {
             name: {type: String, default: 'Unnamed'},
-            tags: {type: Array, default: () =>([])},
-            showElement : false
+            tags: {type: Array, default: () => ([])},
+            visible: false
         },
+        data: () => ({
+            showElement: false
+        }),
         methods: {
-          rotateElement (index) {
-            if(this.tags.length > 0){
-              return ((index * 20)-20)
-            }else {
-              return (0)
+            show() {
+                this.showElement = !this.showElement;
+                this.$emit('selection', this.name)
+            },
+            rotateElement(index) {
+                if (this.tags.length > 0) {
+                    return ((index * 20) - 20)
+                } else {
+                    return (0)
+                }
+            },
+            marginElement(index) {
+                if (index === 1) {
+                    return ('m-[35px]')
+                } else {
+                    return ('m-0')
+                }
             }
-          },
-          marginElement (index) {
-             if(index === 1){
-               return ('m-[35px]')
-          }else{
-            return('m-0')
-          }
         }
-      }
     }
 </script>
 
 <style scoped>
-.point.two {
-      right: 0;
-      top: 20vh;
-      width: 30vh;
-      height: 30vh;
-      background: #2A00FF;
-      filter: blur(20vh);
-    }
+  .point.two {
+    right: 0;
+    top: 20vh;
+    width: 30vh;
+    height: 30vh;
+    background: #2A00FF;
+    filter: blur(20vh);
+  }
 </style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,43 +1,88 @@
 <template>
-    <div class="is-dark flex flex-col ">
-      <div class="py-20 px-3 relative">
-        <h1 class="text-8xl text-center m-14">Projects</h1>
-        <div class="point one flat-1"></div>
-        <ProjectBar name="Farmacia" :tags = "['Healt' , 'SuperMarket', 'Branding']"></ProjectBar>
-        <ProjectBar name="PanuPam" :tags = "['Gym' , 'Home Office']" ></ProjectBar>
-        <ProjectBar name="Clinica Hebe"></ProjectBar>
-        <ProjectBar name="Hamsa"></ProjectBar>
-        <ProjectBar name="Premium Gym"></ProjectBar>
-        <ProjectBar name="Bubble Wafle"></ProjectBar>
-        <ProjectBar name="Resinca"></ProjectBar>
-        <div class="point two flat-1"></div>
-      </div>
+  <div class="is-dark flex flex-col ">
+    <div class="py-20 px-3 relative">
+      <h1 class="text-5xl md:text-8xl text-center m-14">Projects</h1>
+      <div class="point one flat-1"></div>
+      <ProjectBar v-for="(proj, index) of projects" :name="proj.name" :tags="proj.tags" :visible="project === proj.name"
+                  :key="index" @selection="setProject"></ProjectBar>
+      <!--      <ProjectBar name="PanuPam" :tags="['Gym' , 'Home Office']"></ProjectBar>-->
+      <!--      <ProjectBar name="Clinica Hebe"></ProjectBar>-->
+      <!--      <ProjectBar name="Hamsa"></ProjectBar>-->
+      <!--      <ProjectBar name="Premium Gym"></ProjectBar>-->
+      <!--      <ProjectBar name="Bubble Wafle"></ProjectBar>-->
+      <!--      <ProjectBar name="Resinca"></ProjectBar>-->
+      <div class="point two flat-1"></div>
     </div>
+  </div>
 </template>
 
 <script>
     import ProjectBar from "../components/core/ProjectBar";
+
     export default {
         name: "projects",
-        components: {ProjectBar}
+        components: {ProjectBar},
+        data: () => ({
+            project: '',
+            projects: [
+                {
+                    id: 1,
+                    name: 'Farmacia',
+                    tags: ['Healt', 'SuperMarket', 'Branding']
+                },
+                {
+                    id: 2,
+                    name: 'PanuPam',
+                    tags: ['Gym', 'Home Office']
+                },
+                {
+                    id: 3,
+                    name: 'Clinica Hebe',
+                },
+                {
+                    id: 4,
+                    name: 'Hamsa',
+                },
+                {
+                    id: 5,
+                    name: 'Premium Gym',
+                },
+                {
+                    id: 6,
+                    name: 'Bubble Wafle',
+                },
+                {
+                    id: 7,
+                    name: 'Resinca',
+                },
+            ],
+
+        }),
+        methods: {
+            setProject(name) {
+                if (this.project !== name) this.project = name
+                else this.project = ''
+            }
+        }
     }
 </script>
 
 <style scoped>
-.point.one {
-      left: 0;
-      top: 160vh;
-      width: 30vh;
-      height: 30vh;
-      background: #2A00FF;
-      filter: blur(20vh);
-    }
-.point.two {
-      right: 0;
-      top: 20vh;
-      width: 30vh;
-      height: 30vh;
-      background: #2A00FF;
-      filter: blur(20vh);
-    }
+  .point.one {
+    left: 0;
+    top: 160vh;
+    width: 30vh;
+    height: 30vh;
+    background: #2A00FF;
+    filter: blur(20vh);
+  }
+
+  .point.two {
+    right: 0;
+    top: 20vh;
+    width: 30vh;
+    height: 30vh;
+    background: #2A00FF;
+    filter: blur(20vh);
+  }
 </style>
