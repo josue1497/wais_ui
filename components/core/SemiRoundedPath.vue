3f125b111899f575aa3de1c246334a528fff426c
josuermartinezm <78878134+josuermartinezm@users.noreply.github.com>
2022-06-02 19:06:37 -0400

    Nice

---
+++ components/core/SemiRoundedPath.vue
@@ -0,0 +1,38 @@
+<template>
+    <div :style="{width, height, background, color}">
+      <slot/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SemiRoundedPath",
+        props: {
+            heightSize: {type: [Number, String], default: 50},
+            heightUnit: {type: String, default: 'vh'},
+            widthSize: {type: [Number, String], default: 25},
+            widthUnit: {type: String, default: 'vw'},
+            color: {type: String, default: '#1A1A1A'},
+            background: {type: String, default: '#B3EB00'},
+            reverse: {type: Boolean, default: false},
+        },
+        computed: {
+            width() {
+                return `${this.widthSize}${this.widthUnit}`
+            },
+            height() {
+                return `${this.heightSize}${this.heightUnit}`
+            }
+        }
+    }
+</script>
+
+<style>
+  .path {
+    border-radius: 0 10vh 0 10vh;
+  }
+
+  .path-reverse {
+    border-radius: 10vh 0 10vh 0;
+  }
+</style>
--- pages/gym.vue
+++ pages/gym.vue
@@ -27,18 +27,77 @@
         </div>
       </div>
     </div>
-    <div class="relative flex">
+    <div class="relative flex flex-col px-5 ">
+      <div class="w-3/4 opacity-5 h-80">
+        hola
+      </div>
+      <img src="../assets/img/gym/gym2.png" alt="gym 2">
+      <img src="../assets/img/gym/gym3.png" alt="gym 3" class="w-3/4 mx-10">
+    </div>
+    <div class="w-full h-72">
+      Animacion
+    </div>
+    <div class="w-full px-10 py-14">
+      <p class="text-outline font-regular text-[155px]">Selección cromática</p>
+      <div class="relative w-full h-[75vh] flex justify-center items-center">
+        <SemiRoundedPath width-size="473" width-unit="px" height-size="381" height-unit="px" background="#1A1A1A"
+                         color="#FFFFFF" class="absolute path-three flex" reverse>
+          <span class="w-3/4 m-auto text-[20px] font-normal leading-[2vh] text-center">Profesionalismo, seriedad, fuerza.</span>
+        </SemiRoundedPath>
+        <SemiRoundedPath width-size="549" width-unit="px" height-size="459" height-unit="px" background="#FFFFFF"
+                         color="#1A1A1A" class="absolute path-two flex text-center">
+          <span class="w-3/4 m-auto text-[20px] font-normal leading-[2vh] text-center">Positividad, balance, luminosidad. </span>
+        </SemiRoundedPath>
+        <SemiRoundedPath width-size="297" width-unit="px" height-size="256" height-unit="px"
+                         class="absolute path-one flex">
+          <span class="w-1/2 m-auto text-[20px] font-normal leading-[2vh]">Fuerza, vivaz, carácter, intenso, frescura, sano, vivo.</span>
+        </SemiRoundedPath>
+      </div>
+      <div class="relative flex w-3/4  mx-auto">
+        <div class="ml-auto w-1/2">
+          <p class="text-[16px] font-bold">Creative concept</p>
+          <p class="text-[16px] font-normal leading-[2vh] mb-5">
+            Characteristics such as energy, strength, character, speed, movement, visual weight were taken into account,
+            this was important to choose thick fonts with a heavy, strong, rough appearance.
+          </p>
+          <p class="text-[16px] font-normal leading-[2vh]">
+            Like that, it was decisive to design, the type of shapes that make up the graphic identifier, dynamic
+            shapes, some with more aggressive cuts than others, but always maintaining the focus of character and
+            strength.
 
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+    import SemiRoundedPath from "../components/core/SemiRoundedPath";
+
     export default {
-        name: "gym"
+        name: "gym",
+        components: {SemiRoundedPath}
     }
 </script>
 
-<style scoped>
+<style>
+  .path-one {
+    top: -7.5vh;
+    left: 35vh;
+    border-radius: 0 10vh 0 10vh;
+  }
 
+  .path-two {
+    top: -7.5vh;
+    right: 15vh;
+    border-radius: 0 15vh 0 15vh;
+  }
+
+
+  .path-three {
+    top: 10vh;
+    left: 32vh;
+    border-radius: 10vh 0 10vh 0;
+  }
 </style>
