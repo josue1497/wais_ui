5c2b2edfeb7f977e5b26c977b140c655e66653e8
josuermartinezm <78878134+josuermartinezm@users.noreply.github.com>
2022-06-13 14:54:27 -0400

    Nice

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,12 @@
   <div>
     <section class="w-full flex main-banner relative overflow-hidden" @mousemove="updateSpotlight" @mouseleave="leave"
              @mouseenter="enter">
-      <canvas width="32" height="32" class="absolute" id="granim-canvas"></canvas>
+      <!--      <canvas width="32" height="32" class="absolute" id="granim-canvas"></canvas>-->
       <!--      style="transform: scale(2)"-->
+      <div class="super-circle s-circle-1" id="cs-1" style="top: -50%; right: -30%;"></div>
+      <div class="super-circle s-circle-3" id="cs-3" style="bottom: -129%; right: -20%;"></div>
+      <div class="super-circle s-circle-2" id="cs-2" style="left: -70%; top: -20%;"></div>
+<!--      <div class="super-circle s-circle-2" id="cs-6"></div>-->
       <div class="hidden md:block circle-out ">
         <div class="circle circle-in-1" :style="{transform: 'translate3d(40vw, 35vh, 0px) rotate(-25deg)'}"></div>
         <div class="circle circle-in-2" :style="{transform: 'translate3d(40vw, 35vh, 0px) rotate(-25deg)'}"></div>
@@ -11,7 +15,7 @@
         <div class="circle circle-in-4" :style="{transform: 'translate3d(40vw, 35vh, 0px) rotate(-25deg)'}"></div>
         <div class="circle circle-in-5" :style="{transform: 'translate3d(40vw, 35vh, 0px) rotate(-25deg)'}"></div>
       </div>
-      <div class="flex flex-col justify-start items-start my-44 md:my-20 w-full relative">
+      <div class="flex flex-col justify-start items-start my-44 md:my-20 w-full relative" style="z-index: 10">
         <div class="flex flex-col justify-center items-center w-full ">
           <h1
             class="text-[77px] md:text-[180px] lg:text-[200px] xl:text-[277px] mx-auto md:ml-auto leading-[5rem] md:leading-[10rem] lg:leading-[12rem] xl:leading-[15rem] flex flex-col w-3/4 md:w-10/12 lg:w-8/12">
@@ -68,14 +72,23 @@
     <CoreSection items="start" justify="center" id="3" color="#2A00FF" sm-px="0" sm-size="64"
                  class="h-auto md:h-[20rem] lg:h-[30rem] xl:h-[50rem] relative">
       <div class="w-full px-0 md:px-0 text-center flat-2">
-        <h3 class="text-[22px] md:text-[44px] lg:text-[74px] text-center">
+        <h3 class="text-[36px] md:text-[44px] lg:text-[74px] text-center font-normal">
           Our clients
         </h3>
-        <div class="w-full overflow-x-hidden">
+        <div class="w-full overflow-x-hidden hidden md:flex">
           <div class=" p-0 md:px-5 flex marquee-content w-full">
             <div class="p-2 md:p-4 flex justify-center align-center flat-2 marquee-item"
                  v-for="({l, n}, index) of clients" :key="index">
               <img :src="!darkMode? n : l" :alt="!darkMode? n : l" class="mx-auto">
+            </div>
+          </div>
+        </div>
+        <div class="w-full overflow-x-hidden inline md:hidden">
+          <div class=" p-0 md:px-5 block w-full grid grid-cols-2 gap-4">
+            <div :class="`py-4 px-8 md:p-4 justify-center align-center flat-2 w-full flex`"
+                 v-for="({l, n, span}, index) of smClients" :key="index">
+              <!--                 ${span ? 'col-span-2':''}-->
+              <img :src="!darkMode? n : l" :alt="!darkMode? n : l" class="">
             </div>
           </div>
         </div>
@@ -101,6 +114,15 @@
             })
         },
         data: () => ({
+            smClients: [
+                {l: require(`../assets/img/clients/century-l.svg`), n: require(`../assets/img/clients/century-n.svg`)},
+                {l: require(`assets/img/clients/check-l.svg`), n: require(`assets/img/clients/check-n.svg`)},
+                {l: require(`../assets/img/clients/pastora-l.svg`), n: require(`../assets/img/clients/pastora-n.svg`)},
+                {l: require(`../assets/img/clients/hebe-l.svg`), n: require(`../assets/img/clients/hebe-n.svg`)},
+                {l: require(`../assets/img/clients/nikk-l.svg`), n: require(`../assets/img/clients/nikk-n.svg`)},
+                {l: require(`../assets/img/clients/resinca-l.svg`), n: require(`../assets/img/clients/resinca-n.svg`)},
+
+            ],
             clients: [
                 {l: require(`../assets/img/clients/century-l.svg`), n: require(`../assets/img/clients/century-n.svg`)},
                 {l: require(`assets/img/clients/check-l.svg`), n: require(`assets/img/clients/check-n.svg`)},
@@ -122,7 +144,11 @@
                 {l: require(`../assets/img/clients/resinca-l.svg`), n: require(`../assets/img/clients/resinca-n.svg`)},
             ],
             showPointer: false,
+            animationInterval: null,
         }),
+        async mounted(){
+            //this.startAnimation()
+        },
         methods: {
             enter(e) {
                 this.showPointer = true;
@@ -132,6 +158,46 @@
             },
             timeout(ms) {
                 return new Promise(resolve => setTimeout(resolve, ms));
+            },
+            startAnimation(){
+                const vm = this;
+                const sc1 = document.querySelector('#cs-1');
+                const sc2 = document.querySelector('#cs-2');
+                const sc3 = document.querySelector('#cs-3');
+                // const sc6 = document.querySelector('#cs-6');
+                this.animationInterval = setInterval(() => {
+                    sc1.style.top = `${vm.generateRandom(100)}%`;
+                    sc1.style.left = `${vm.generateRandom(50)}%`;
+
+                    sc2.style.top = `${vm.generateRandom(100)}%`;
+                    sc2.style.right = `${vm.generateRandom(100)}%`;
+
+                    sc3.style.bottom = `${vm.generateRandom(100)}%`;
+                    sc3.style.left = `${vm.generateRandom(100)}%`;
+
+                    sc4.style.bottom = `${vm.generateRandom()}%`;
+                    sc4.style.right = `${vm.generateRandom()}%`;
+                    //
+                    sc5.style.top = `${vm.generateRandom(100)}%`;
+                    sc5.style.right = `${vm.generateRandom(100)}%`;
+                    //
+                    // sc6.style.top = `${vm.generateRandom(60)}%`;
+                    // sc6.style.left = `${vm.generateRandom(60)}%`;
+                }, 1000)
+            },
+            generateRandom(maxLimit = 50) {
+                let rand = Math.random() * maxLimit;
+                console.log(rand); // say 99.81321410836433
+
+                rand = Math.floor(rand); // 99
+
+                const plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+
+
+                return plusOrMinus ? rand * -1 : rand;
+            },
+            generateRandomZIndex(maxLimit = 25) {
+                return Math.floor(Math.round(Math.random()* maxLimit));
             },
             async updateSpotlight(e) {
                 const spotlight = document.querySelectorAll(`.circle`)
@@ -144,7 +210,10 @@
                 }
             }
         },
-        mixins: [aosMixin]
+        mixins: [aosMixin],
+        beforeDestroy() {
+            clearInterval(this.animationInterval)
+        }
     }
 
 </script>
@@ -198,6 +267,80 @@
     width: 100%;
   }
 
+  .s-circle-1 {
+    background: #99FCCE;
+    width: 180vh;
+    height: 180vh;
+    /*left: -19.64px;*/
+    /*top: -91.02px;*/
+  }
+
+  .s-circle-2 {
+    background: #F7633F;
+    width: 220vh;
+    height: 220vh;
+    /*left: -65px;*/
+    /*top: 62px;*/
+  }
+
+  .s-circle-3 {
+    background: #B749E6;
+    width: 200vh;
+    height: 200vh;
+    /*left: 78px;*/
+    /*top: 58px;*/
+    /*z-index: 1;*/
+    /*animation: super-circle infinite 4s;*/
+
+  }
+
+  .super-circle {
+    position: absolute;
+    /*width: 160vh;*/
+    /*height: 160vh;*/
+    filter: blur(25vh);
+    /*-webkit-box-shadow: inset 6px 2px 44px 20px #FFFFFF;*/
+    /*box-shadow: inset 6px 2px 44vh 20px #FFFFFF;*/
+    border-radius: 100vh;
+    transition: all 5s ease-in-out;
+  }
+
+  @keyframes super-circle {
+    0% {
+      transform: translate3d(10vh, 10vh, 0);
+    }
+    10% {
+      transform: translate3d(20vh, 20vh, 0);
+    }
+    20% {
+      transform: translate3d(30vh, 30vh, 0);
+    }
+    30% {
+      transform: translate3d(40vh, 40vh, 0);
+    }
+    40% {
+      transform: translate3d(50vh, 50vh, 0);
+    }
+    50% {
+      transform: translate3d(60vh, 60vh, 0);
+    }
+    60% {
+      transform: translate3d(70vh, 70vh, 0);
+    }
+    70% {
+      transform: translate3d(80vh, 80vh, 0);
+    }
+    80% {
+      transform: translate3d(80vh, 80vh, 0);
+    }
+    90% {
+      transform: translate3d(90vh, 90vh, 0);
+    }
+    100% {
+      transform: translate3d(100vh, 100vh, 0);
+    }
+  }
+
   @keyframes scroll-marquee {
     0% {
       transform: translateX(0);
@@ -209,7 +352,6 @@
 
   .GFG {
     color: transparent;
-    /*font-size: 50px;*/
     -webkit-text-stroke-width: 1px;
     -webkit-text-stroke-color: black;
   }
@@ -226,10 +368,10 @@
   .customer::after {
     content: "";
     background: #fff;
-    height: 10px;
+    height: 10%;
     border-bottom: 0;
     position: absolute;
-    top: 8vh;
+    top: 50%;
     left: 0;
     animation: line infinite 4s;
   }
@@ -456,6 +598,5 @@
   .icon-bar a:hover {
     transform: translateY(-10px);
   }
-
 
 </style>
