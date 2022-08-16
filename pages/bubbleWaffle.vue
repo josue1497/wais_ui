56990b93bff8b384479ea00b857d8d2a920e7dde
goyo1393 <jgescobarquintero@gmail.com>
2022-08-15 20:54:59 -0400

    BubbleWaffle Structure

---
+++ pages/bubbleWaffle.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="is-dark flex flex-col">
+    <div class="inline-block text-center relative">
+      <img
+        src="../assets/img/bubbleWaffle/Rectangle.png"
+        class="w-full md:w-1/2 md:mx-auto md:mb-20 mt-32"
+      />
+      <div
+        class="
+          p-12
+          block
+          z-index-1
+          text-black
+          dark:text-white
+          flex
+          justify-center
+          ml-[50px]
+        "
+      >
+        <div>
+          <h1
+            class="text-[14px] md:text-[20px] lg:text-[24px] px-[75px]"
+            style="text-align: start"
+          >
+            Trade Item
+          </h1>
+          <div class="flex px-[80px] py-8">
+            <p>Gastronomy</p>
+          </div>
+        </div>
+        <div>
+          <h1
+            class="text-[14px] md:text-[20px] lg:text-[24px] px-[75px]"
+            style="text-align: start"
+          >
+            Service Category
+          </h1>
+          <div class="flex px-[75px] py-8">
+            <p>Branding and graphic design</p>
+          </div>
+        </div>
+        <div>
+          <h1
+            class="text-[14px] md:text-[20px] lg:text-[24px] px-[75px]"
+            style="text-align: start"
+          >
+            Service Provider
+          </h1>
+          <div class="flex px-[75px] py-8" style="text-align: start">
+            <p>
+              Logo <br />
+              Design of brand accessories
+            </p>
+          </div>
+        </div>
+      </div>
+      <img src="../assets/img/bubbleWaffle/logos_bubble_waffle_2 1.png" />
+      <img src="../assets/img/bubbleWaffle/conceptos_bubble_waffle_1 2.png" />
+      <div class="w-[100%] h-[1900px] bg-[#420951]">
+        <div class="container flex flex-col justify-center colortext-white">
+          <img
+            src="../assets/img/bubbleWaffle/Bubble_waffle_BE_english-02.png"
+          />
+          <img
+            src="../assets/img/bubbleWaffle/Bubble_waffle_BE_english-04.png"
+          />
+        </div>
+      </div>
+      <div class="w-[100%] h-[1250px] bg-[#F8FFF8]">
+      <div class="mt-[-200px] z-index">
+        <img src="../assets/img/bubbleWaffle/BubbleWaffle05e.png" class="w-[60%]" />
+      </div>
+
+       <div class="flex justify-end mt-[50px]">
+        <img src="../assets/img/bubbleWaffle/BubbleWaffle06.png" class="w-[60%]" />
+      </div>
+      </div>
+
+       <div class="w-[100%] h-[950px] bg-[#DAD0E4] mb-[200px]">
+      <div class="w-[75%]">
+        <img src="../assets/img/bubbleWaffle/BubbleWaffle07.png" class="w-[60%]" />
+      </div>
+
+       <div class="flex justify-end mt-[50px] z-index mt-[-100px] mr-[100px]">
+        <img src="../assets/img/bubbleWaffle/BubbleWaffle08.png" class="w-[35%] h-[35%]" />
+      </div>
+      </div>
+
+    </div>
+  </div>
+</template>
