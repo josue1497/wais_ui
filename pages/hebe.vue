1f7075f6a0a94f501d1fd879ea1da2d7a99fd6f7
goyo1393 <jgescobarquintero@gmail.com>
2022-12-04 18:16:55 -0400

    Hebe View

---
+++ pages/hebe.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="is-dark flex flex-col">
+        <div class="inline-block text-center ml-[350px]">
+              <img
+                src="../assets/img/hebe/hebe1.png"
+                class="w-full md:w-4/5 md:mx-auto md:mb-20 mt-32"
+              />
+        </div>
+        <div class="p-12 block z-index-1 text-black dark:text-white flex justify-center ml-[350px] ">
+              <div>
+                  <h1 class="text-[14px] md:text-[20px] lg:text-[24px] px-[75px]" style="text-align: start" >
+                    Trade Item
+                  </h1>
+                  <div class="flex px-[120px] py-8">
+                    <p>Health</p>
+                  </div>
+              </div>
+              <div>
+                  <h1 class="text-[14px] md:text-[20px] lg:text-[24px] px-[75px]" style="text-align: start">
+                    Service Category
+                  </h1>
+                  <div class="flex px-[75px] py-8">
+                    <p>
+                      Analysis and strategy Content creation Web design
+                      <br/>
+                      Marketing in search engines Social marketing Branding and graphic design
+                    </p>
+                  </div>
+              </div>
+              <div>
+                  <h1 class="text-[14px] md:text-[20px] lg:text-[24px] px-[75px]" style="text-align: start">
+                    Service Provider
+                  </h1>
+                  <div class="flex px-[75px] py-4" style="text-align: start">
+                    <p>
+                      Logo
+                      <br/>
+                      Website design and development (Wordpress)
+                      <br/>
+                      Logo
+                      <br/>
+                      Social media management.
+                      <br/>
+                      Design for digital media.
+                      <br/>
+                      Graphic concept for instagram.
+                      <br/>
+                        SEO
+                    </p>
+                  </div>
+            </div>
+      </div>
+          <div class="bg-[#C7C7C7] h-[1290px]">
+              <img src="../assets/img/hebe/hebe7.png" alt="logo-01" class="m-16 h-1/4">
+              <img src="../assets/img/hebe/Clinica-Hebe-BE-03.png" alt="content-hebe" class="w-4/5 ml-auto">
+          </div>
+
+          <div class="bg-[#C7C7C7] h-[850px] w-[700px] m-40">
+                <img src="../assets/img/hebe/Clinica-Hebe-BE-04.png" alt="hebe-tablet">
+                <img src="../assets/img/hebe/hebe2.png" alt="hebe-phones" class="m-auto">
+          </div>
+
+          <div>
+            <img src="../assets/img/hebe/hebe3.png" alt="clinic-hebe">
+          </div>
+
+          <div class="bg-[#2C9F9B] h-[1200px] mt-32 mb-64">
+            <img src="../assets/img/hebe/hebe4.png" alt="doctor-hebe" class="ml-auto">
+            <img src="../assets/img/hebe/hebe5.png" alt="hebe-car" class="m-auto mt-40">
+            <img src="../assets/img/hebe/hebe6.png" alt="hebe-car" class="m-auto mt-40 ml-[700px]">
+          </div>
+      </div>
+
+</template>
