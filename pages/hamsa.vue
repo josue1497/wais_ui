b895b0d7b758122ab7212eaf0118430286efd1f1
goyo1393 <jgescobarquintero@gmail.com>
2022-12-04 19:18:16 -0400

    Hamsa View

---
+++ pages/hamsa.vue
@@ -0,0 +1,48 @@
+<template>
+      <div class="is-dark flex flex-col">
+          <div class="flex text-center items-center justify-center mt-40">
+              <img src="../assets/img/hamsa/hamsa_2.png" alt="baneer_2" class="w-1/2 h-[400px]">
+              <img src="../assets/img/hamsa/hamsa_1.png" alt="baneer_1" class="w-1/2 h-[400px]">
+          </div>
+          <div class="p-12 block z-index-1 text-black dark:text-white flex justify-start m-16">
+              <div>
+                  <h1 class="text-[14px] md:text-[20px] lg:text-[24px] px-[75px] font-bold" style="text-align: start">
+                    Trade Item
+                  </h1>
+                  <div class="flex px-[80px] py-8">
+                    <p>Beauty</p>
+                  </div>
+              </div>
+              <div>
+                  <h1 class="text-[14px] md:text-[20px] lg:text-[24px] px-[75px] font-bold" style="text-align: start">
+                    Service category
+                  </h1>
+                  <div class="flex px-[80px] py-8">
+                    <p>Branding and graphic design</p>
+                  </div>
+              </div>
+              <div>
+                  <h1 class="text-[14px] md:text-[20px] lg:text-[24px] px-[75px] font-bold" style="text-align: start">
+                    Services provided
+                  </h1>
+                  <div class="flex px-[80px] py-8">
+                    <p>
+                      Logo
+                      <br/>
+                      Video
+                    </p>
+                  </div>
+              </div>
+          </div>
+          <img src="../assets/img/hamsa/hamsa_3.png" alt="styles_hamsa" class="w-3/5 ml-48">
+          <div class="bg-[#D6D7A9] h-[1400px] mt-36 mb-36">
+                <img src="../assets/img/hamsa/hamsa_4.png" alt="hamsa_fonts" class="ml-auto w-4/6">
+                <img src="../assets/img/hamsa/hamsa_5.png" alt="hamsa_styles" class="mr-auto w-1/2 mt-20">
+                <img src="../assets/img/hamsa/hamsa_6.png" alt="hamsa_styles" class="ml-auto w-1/3 h-1/4 mt-28 mr-96">
+          </div>
+          <img src="../assets/img/hamsa/hamsa_7.png" alt="hamsa-styles-2" class="mt-40 mb-40">
+          <div class="flex  justify-center">
+                <img src="../assets/img/hamsa/hamsa_8.png" alt="hamsa-styles-3" class="w-1/2">
+          </div>
+      </div>
+</template>
