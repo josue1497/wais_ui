e93f2247a3159ba53c128459369c3e259e5349e8
goyo1393 <jgescobarquintero@gmail.com>
2022-12-04 20:23:31 -0400

    Pamypan View

---
+++ pages/pamypan.vue
@@ -0,0 +1,60 @@
+<template>
+       <div class="is-dark flex flex-col">
+            <div class="p-12 block z-index-1 text-black dark:text-white flex justify-start mt-24 ml-12">
+                  <div>
+                      <h1 class="text-[14px] md:text-[20px] lg:text-[24px] px-[75px] font-bold" style="text-align: start">
+                        Trade Item
+                      </h1>
+                      <div class="flex px-[80px] py-8">
+                        <p>Gastronomy</p>
+                      </div>
+                  </div>
+                  <div>
+                      <h1 class="text-[14px] md:text-[20px] lg:text-[24px] px-[75px] font-bold" style="text-align: start">
+                        Service category
+                      </h1>
+                      <div class="flex px-[80px] py-8">
+                        <p>Branding and graphic design</p>
+                      </div>
+                  </div>
+                  <div>
+                      <h1 class="text-[14px] md:text-[20px] lg:text-[24px] px-[75px] font-bold" style="text-align: start">
+                        Services provided
+                      </h1>
+                      <div class="flex px-[80px] py-8">
+                          <p>
+                            Illustration
+                            <br>
+                            Social media management.
+                            <br>
+                            Packaging design
+                            <br>
+                            Content creation for social networks.
+                            <br>
+                            Graphic concept for instagram
+                          </p>
+                      </div>
+                  </div>
+              </div>
+              <div class="flex text-center items-center justify-center">
+                 <img src="../assets/img/pamypan/pamypan-1.png" alt="baneer_2" class=" w-4/5">
+              </div>
+              <div class="bg-[#D7D4A1] h-[1250px] w-4/5 m-auto mb-32">
+                <img src="../assets/img/pamypan/vector.png" alt="logo" class="mr-auto ml-24 mt-4">
+                <img src="../assets/img/pamypan/pamypan-2.png" alt="pamypan-pan" class="mx-auto w-3/4 mt-32">
+                <img src="../assets/img/pamypan/pamypan-3.png" alt="pamypan-box">
+              </div>
+              <div class="bg-[#F3A835] h-[400px] w-3/5 ml-auto mr-4">
+                  <img src="../assets/img/pamypan/pamypan-5.png" alt="pamypan-env" class="w-1/2">
+              </div>
+              <div class="mt-24 mx-4">
+                <img src="../assets/img/pamypan/pamypan-6.png" alt="pamypan-mrcy">
+              </div>
+              <div class="bg-[#DEDAD9] mx-4 h-[800px]">
+                <img src="../assets/img/pamypan/pamypan-7.png" alt="pamypan-cups" class="m-auto w-3/4">
+              </div>
+              <div class="mt-12">
+                <img src="../assets/img/pamypan/pamypan-8.png" alt="pamypan-stikers" class="m-auto w-3/5 mb-24">
+              </div>
+       </div>
+</template>
